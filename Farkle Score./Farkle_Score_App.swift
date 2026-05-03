a7dd0e988d7f86670e8eee12b102ff092f8f2d48
fennelouski <nathanfennel@gmail.com>
2026-05-03 22:17:38 +0200

    Initial Commit

---
+++ Farkle Score./Farkle_Score_App.swift
@@ -0,0 +1,32 @@
+//
+//  Farkle_Score_App.swift
+//  Farkle Score.
+//
+//  Created by Nathan Fennel on 5/3/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Farkle_Score_App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Farkle Score.Tests/Farkle_Score_Tests.swift
@@ -0,0 +1,18 @@
+//
+//  Farkle_Score_Tests.swift
+//  Farkle Score.Tests
+//
+//  Created by Nathan Fennel on 5/3/26.
+//
+
+import Testing
+
+struct Farkle_Score_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
